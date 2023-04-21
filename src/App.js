898be1d40f78897a31f2f586a18b8c6c031354ea
Kevin Stajurski <kevinstajurski@gmail.com>
2023-04-20 22:40:05 -0300

    se agrego router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import { CartWidget } from './components/CartWidget/CartWidget';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { NavBar } from './components/NavBar/NavBar';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      <NavBar>
-        <CartWidget/>
-      </NavBar>
-      <ItemListContainer/>
-      <ItemDetailContainer/>
+      <BrowserRouter>
+          <NavBar>
+            <CartWidget/>
+          </NavBar>
+        <Routes>
+          <Route path='/' element={<ItemListContainer/>}/>
+          {/* <Route path='category/:id' element={<ItemLIstContainer+id/>}/> */}
+          <Route path='item/:id' element={<ItemDetailContainer/>}/>
+          {/* <Route path='cart' element={<Cart/>}/>
+          <Route path='checkout' element={<CheckOut/>}/> */}
+          <Route path='*' element={<Navigate to = '/'/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import logo from '../../logo.png'
 import "./NavBar.css"
 import { CartWidget } from '../CartWidget/CartWidget'
+import { Link } from 'react-router-dom'
 export const NavBar = () => {
     return (
         <div className='NavBar'>
-            <img src={logo} alt="logo de la tienda" className='Logo' />
-            <a href='google.com'>Mouse</a>
-            <a href='google.com'>Teclados</a>
-            <a href='google.com'>Auriculares</a>
+            <Link to={'/'}>
+                <img src={logo} alt="logo de la tienda" className='Logo' />
+            </Link>
+            <Link to={'/'}>Mouse</Link>
+            <Link to={'/'}>Teclados</Link>
+            <Link to={'/'}>Auriculares</Link>
             <CartWidget/>
         </div>
     )
